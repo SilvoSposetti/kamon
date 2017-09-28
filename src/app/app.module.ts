9c63906016ceb440b0e5b82bda44532193e81817
Silvo Sposetti <silvosposetti@gmail.com>
2017-09-28 17:46:01 +0200

    added content, background and clock components
    content is on the standard value of z-index (0) while background stays
    one level behind (-1).
    +added examples for variables in styles.css
    + added loading gif

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { BackgroundComponent } from './background/background.component';
+import { ContentComponent } from './content/content.component';
+import { ClockComponent } from './content/clock/clock.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BackgroundComponent,
+    ContentComponent,
+    ClockComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/background/background.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-background',
+  templateUrl: './background.component.html',
+  styleUrls: ['./background.component.css']
+})
+export class BackgroundComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
