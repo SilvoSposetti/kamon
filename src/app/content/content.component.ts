f067a0b1c35ed378c17ef9f5c9580f48d6da5701
Silvo Sposetti <silvosposetti@gmail.com>
2017-11-06 19:18:41 +0100

    forgot to set a method as public

--- src/app/content/content.component.ts
+++ src/app/content/content.component.ts
@@ -72,7 +72,7 @@ export class ContentComponent implements OnInit {
     });
   }
 
-  private searchInputChanged(): void {
+  public searchInputChanged(): void {
     this.searchService.setSearchString(this.searchText); // Update content of searchString
     this.checkShowSearch();
   }
