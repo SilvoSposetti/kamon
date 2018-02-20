2369d5b0464255eb1a5a8dde764d5adcb93621ac
Silvo Sposetti <silvosposetti@gmail.com>
2018-02-20 13:17:16 +0100

    improved and fixed game of life scene
    turns out assignment operator in typescript/javascript passes only a
    reference.
    Fixed bug by marking down which positions are to swap.

--- src/app/background/scene/game-of-life/game-of-life.component.ts
+++ src/app/background/scene/game-of-life/game-of-life.component.ts
@@ -27,15 +27,16 @@ export class GameOfLifeComponent implements OnInit, OnDestroy {
 
   private running: boolean;
 
-  private columns: number = 200;
+  private columns: number = 202;
   private rows: number;
   private cellSize: number;
   private grid: boolean[][] = [];
-  private updateEachAmountOfFrame: number = 5;
+  private updateEachAmountOfFrame: number = 3;
   private frameCount: number = 0;
+  private frameDrawnCount: number = 0;
   private paddingValue: boolean = false;
 
-  // each element of the grid contains 0 for black and 1 for white.
+  // each element of the grid contains false for black and true for white.
 
   constructor() {
   }
@@ -54,65 +55,77 @@ export class GameOfLifeComponent implements OnInit, OnDestroy {
     this.updateFps();
     if (this.frameCount === 0) {
       this.drawBackground();
-      this.drawGrid();
+      // do update before draw so that the first 'setup' is not drawn.
       this.update();
+      this.drawGrid();
+      this.frameDrawnCount++;
     }
 
     this.frameCount++;
     this.frameCount %= this.updateEachAmountOfFrame;
-
+    // Stop the scene after some iterations:
+    //if (this.frameDrawnCount === 1200) {
+    //  this.running = false;
+    //}
     if (this.running) {
       requestAnimationFrame(() => this.loop());
     }
   }
 
   private setup(): void {
+    // Variables initialization:
     this.cellSize = this.screenWidth / this.columns;
     this.rows = Math.ceil(this.screenHeight / this.cellSize);
     let value: boolean;
-    let firstAndLastRow: boolean[] = [];
+    // grid is generated with one cell as padding in each directions.
+    let firstAndLastRow: boolean[] = []; // Used as first and last padding rows
     for (let i = 0; i < this.columns + 2; i++) {
       firstAndLastRow.push(this.paddingValue);
     }
-    this.grid.push(firstAndLastRow);
+    this.grid.push(firstAndLastRow); // push first padding row
     for (let i = 0; i < this.rows; i++) {
       let row = [];
-      row.push(this.paddingValue);
+      row.push(this.paddingValue); // push first padding cell of the row
       for (let j = 0; j < this.columns; j++) {
         // Select random number
-        value = Math.random() > 0.9;
+        // value = Math.random() > 0.8;
+
+        value = (i * j) % 3 === 0;
         row.push(value);
       }
-      row.push(this.paddingValue);
+      row.push(this.paddingValue); // Push last padding cell of the row
       this.grid.push(row);
     }
-    this.grid.push(firstAndLastRow);
+    this.grid.push(firstAndLastRow); // push last padding row
   }
 
   private update(): void {
-    let newGrid = this.grid;
-    for (let i = 1; i < this.rows+1; i++) {
-      for (let j = 1; j < this.columns+1; j++) {
+    let positionsToModify: number[][] = [];
+    for (let i = 1; i < this.rows + 1; i++) { // start from 1 and ends at +1 because of padding
+      for (let j = 1; j < this.columns + 1; j++) { // start from 1 and ends at +1 because of padding
         let neighbours = this.checkNeighbours(i, j);
         if (this.grid[i][j]) { // Cell is alive.
           if (neighbours <= 1) { // 1: Any live cell with fewer than two live neighbours dies, as if caused by underpopulation.
-            newGrid[i][j] = false;
+            positionsToModify.push([i, j]);
           }
           else if (neighbours >= 4) { // 2: Any live cell with more than three live neighbours dies, as if by overpopulation.
-            newGrid[i][j] = false;
+            positionsToModify.push([i, j]);
           }
+          // not really needed
           //else { // 3: Any live cell with two or three live neighbours lives on to the next generation.
-            //newGrid[i][j] = true;
+          //newGrid[i][j] = true;
           //}
         }
         else { // Cell is dead.
           if (neighbours === 3) { // 4: Any dead cell with exactly three live neighbours becomes a live cell, as if by reproduction.
-            newGrid[i][j] = true;
+            positionsToModify.push([i, j]);
           }
         }
       }
     }
-    this.grid = newGrid;
+    for (let k = 0; k < positionsToModify.length; k++) {
+      this.grid[positionsToModify[k][0]][positionsToModify[k][1]] = !this.grid[positionsToModify[k][0]][positionsToModify[k][1]];
+    }
   }
 
   private drawBackground(): void {
@@ -126,8 +139,8 @@ export class GameOfLifeComponent implements OnInit, OnDestroy {
   private drawGrid(): void {
     let ctx: CanvasRenderingContext2D = this.canvasRef.nativeElement.getContext('2d');
     ctx.fillStyle = '#aaaaaa';
-    for (let i = 1; i < this.rows + 1; i++) {
-      for (let j = 1; j < this.columns + 1; j++) {
+    for (let i = 1; i < this.rows + 1; i++) { // start from 1 and ends at +1 because of padding
+      for (let j = 1; j < this.columns + 1; j++) { // start from 1 and ends at +1 because of padding
         if (this.grid[i][j]) {
           ctx.fillRect(this.cellSize * (j - 1), this.cellSize * (i - 1), this.cellSize, this.cellSize);
         }
